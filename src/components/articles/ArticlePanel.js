42e3eefeecd2b43572e78f89b5a8b3da78563369
Begona Fernandez <boadicea1979@yahoo.es>
2019-05-24 13:22:23 +0100

    deleted unnecessary console.log

--- src/components/articles/ArticlePanel.js
+++ src/components/articles/ArticlePanel.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const ArticlePanel = ({ article, h1}) => {
-  console.log(article)
   const {id, title, content} = article
   return(
     <div className="container">
