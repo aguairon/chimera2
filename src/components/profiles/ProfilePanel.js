50e9b2b3a841a160d9aa660978ddfd2c8614aaa6
Begona Fernandez <boadicea1979@yahoo.es>
2019-05-31 15:13:13 +0100

    refactored Profile Panel

--- src/components/profiles/ProfilePanel.js
+++ src/components/profiles/ProfilePanel.js
@@ -8,7 +8,7 @@ const ProfilePanel = ({ id, username, email, created_articles: created }) => {
         <article className="tile article is-child notification is-danger">
           <p className="title">{username}</p>
           <p>{email}</p>
-          <p>Wrote {created.length} {created.length === 1? 'article' : 'articles'}</p>
+          <p>Wrote {created.length} article{created.length === 1? '' : 's'}</p>
         </article>
       </Link>
     </div>
