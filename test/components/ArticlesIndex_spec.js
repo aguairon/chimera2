52d366c60b551d91e1bbe4c551133e16e0d57732
Begona Fernandez <boadicea1979@yahoo.es>
2019-06-04 21:22:53 +0100

    extended tests for Article Index

--- test/components/ArticlesIndex_spec.js
+++ test/components/ArticlesIndex_spec.js
@@ -20,7 +20,7 @@ describe('ArticlesIndex test', ()=> {
           created: '2019-03-04 16:01:38',
           creator: {
             email: 'begona@gmail.com',
-            id: 2,
+            id: 3,
             username: 'begona'
           },
           id: 2,
@@ -34,10 +34,6 @@ describe('ArticlesIndex test', ()=> {
 
     sinon.stub(axios, 'get').returns(response)
     done()
-    after(done => {
-      axios.get.restore()
-      done()
-    })
   })
 
   beforeEach(done => {
@@ -49,10 +45,41 @@ describe('ArticlesIndex test', ()=> {
     done()
   })
 
+  after(done => {
+    axios.get.restore()
+    done()
+  })
+
   it('should create the correct state', done => {
     response.then(() => {
       wrapper.update()
-      expect(wrapper.find('ArticlesIndex').state().articles[0].id).to.eq(2)
+      const indexState  = wrapper.find('ArticlesIndex').state()
+      const article = indexState.articles[0]
+      expect(indexState.articles.length).to.eq(1)
+      expect(article.id).to.eq(2)
+      expect(article.title).to.eq('Athos')
+      expect(article.updated_at).to.eq('2019-03-04 16:01:38')
+      expect(article.content).to.eq('blah')
+      expect(article.created).to.eq('2019-03-04 16:01:38')
+      expect(article.creator.email).to.eq('begona@gmail.com')
+      expect(article.creator.id).to.eq(3)
+      expect(article.creator.username).to.eq('begona')
+      expect(article.liked_by).to.eq('')
+      expect(article.messages).to.eq('')
+      expect(indexState.search).to.eq('')
+      expect(indexState.error).to.eq('')
+      done()
+    })
+  })
+
+  it('should render the correct HTML', done => {
+    response.then(() => {
+      wrapper.update()
+      expect(wrapper.find('.section').length).to.eq(1)
+      expect(wrapper.find('h1').length).to.eq(1)
+      expect(wrapper.find('ArticleSearchBar').length).to.eq(1)
+      expect(wrapper.find('ArticlePanel').length).to.eq(1)
+      expect(wrapper.find('ArticlePanel').prop('article')).to.eq(wrapper.find('ArticlesIndex').state().articles[0])
       done()
     })
   })
