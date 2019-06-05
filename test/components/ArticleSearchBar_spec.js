4cdc010b76d2b77b969333dd3ab1901fed753e2f
Begona Fernandez <boadicea1979@yahoo.es>
2019-06-05 17:09:00 +0100

    wrote tests for artcile search bar

---
+++ test/components/ArticleSearchBar_spec.js
@@ -0,0 +1,35 @@
+/* global describe, it, beforeEach */
+
+import React from 'react'
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
+import sinon from 'sinon'
+import ArticleSearchBar from '../../src/components/articles/ArticleSearchBar'
+// import ArticlesIndex from '../../src/components/articles/ArticlesIndex'
+
+describe('ArticleSearchBar Login test when no data is provided', () => {
+  let wrapper
+
+  const onSubmit = sinon.stub()
+  beforeEach(done => {
+    wrapper = shallow(<ArticleSearchBar handleChange={onSubmit}/>)
+    done()
+  })
+
+  it('should render the correct HTML', done => {
+    expect(wrapper.find('form .searchbar').length).to.eq(1)
+    expect(wrapper.find('form .searchbar input').length).to.eq(1)
+    done()
+  })
+
+  it('should render the correct placeholder', done => {
+    expect(wrapper.find('form .searchbar input').prop('placeholder')).to.eq('Search')
+    done()
+  })
+
+  it('responds to value change', done => {
+    wrapper.find('form .searchbar input').simulate('change', { target: { value: 'Hello' }})
+    expect(onSubmit.calledOnce).to.be.true
+    done()
+  })
+})
